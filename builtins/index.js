9194c1a36bb6e646bce1d86b4ca3a3edbf08002a
Brandon Benvie <brandon@bbenvie.com>
2012-10-24 23:53:17 -0400

    fix relative dirname in node

--- builtins/index.js
+++ builtins/index.js
@@ -1,6 +1,6 @@
 var fs = require('fs')
-module.exports = fs.readdirSync('./').map(function(name){
+module.exports = fs.readdirSync(__dirname).map(function(name){
   if (name !== 'index.js') {
-    return fs.readFileSync('./'+name, 'utf8');
+    return fs.readFileSync(__dirname+'/'+name, 'utf8');
   }
 }).filter(Boolean);
