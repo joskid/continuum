23e6ac445818e7bfc2c52e0283e2814858ff3be4
Brandon Benvie <brandon@bbenvie.com>
2012-11-01 18:46:21 -0400

    bunch of improvements to the inspector

--- debugger/debugger.js
+++ debugger/debugger.js
@@ -72,6 +72,7 @@ define(Component.prototype, [
     return this.element.dispatchEvent(event);
   },
   function append(subject){
+    if (!subject) return subject;
     if (subject.element) {
       this.children || (this.children = []);
       this.children.push(subject);
@@ -81,6 +82,19 @@ define(Component.prototype, [
     }
     return subject;
   },
+  function insert(subject, before){
+    if (!this.children) {
+      return this.append(subject);
+    }
+    if (subject instanceof Element) {
+      subject = new Component(subject);
+    }
+    var index = this.children.indexOf(before);
+    if (~index) {
+      this.children.splice(index, 0, subject);
+      this.element.insertBefore(subject.element, before.element);
+    }
+  },
   function remove(subject){
     if (subject === undefined) {
       this.element.parentNode.removeChild(this.element);
@@ -89,6 +103,20 @@ define(Component.prototype, [
         subject = subject.element;
       }
       this.element.removeChild(subject);
+    }
+  },
+  function replace(child, replacement){
+    if (this.children) {
+      return false;
+    }
+    if (replacement instanceof Element) {
+      replacement = new Component(replacement);
+    }
+    var index = this.children.indexOf(child);
+    if (~index) {
+      this.children.splice(index, 1, replacement);
+      this.element.insertBefore(replacement.element, child.element);
+      this.element.removeChild(child.element);
     }
   },
   function width(value){
@@ -147,6 +175,12 @@ define(Component.prototype, [
   },
   function clear(){
     this.innerHTML = '';
+  },
+  function hide(){
+    this.element.style.display = 'none';
+  },
+  function show(){
+    this.element.style.display = '';
   }
 ]);
 
@@ -531,7 +565,7 @@ var Splitter = (function(){
     function drop(e){
       this.removeClass('dragging');
     },
-    function refresh(){
+    function render(){
       this.set(this.lastNear, this.parentSize(), true);
     },
     function set(near, container, silent){
@@ -546,23 +580,10 @@ var Splitter = (function(){
       if (this.lastNear !== near || this.lastFar !== far) {
         this.lastNear = near;
         this.lastFar = far;
-        //this.nearSize(near);
-        //this.farSize(far);
-
         this.near.size = near / container;
         this.far.size = 'auto';
         this.parent.recalc();
         this.parent.resize();
-
-        // if (!silent) {
-        //   this.emit('change-split', {
-        //     change: near - far,
-        //     near: near,
-        //     far: far,
-        //     percent: (near / container * 100 | 0) / 100,
-        //     bubbles: false
-        //   });
-        // }
       }
     },
   ]);
@@ -858,11 +879,15 @@ function Property(mirror, key){
   this.mirror = mirror;
   this.attrs = attributes[mirror.propAttributes(key)];
   this.addClass('property');
+  this._key = key;
   this.key = new Key(key);
   this.key.addClass(this.attrs);
   this.append(this.key);
-  this.property = mirror.get(key);
-  this.append(renderer.render(this.property));
+  this.prop = mirror.get(key);
+  this.property = renderer.render(this.prop);
+  this.append(this.property);
+  this.append(new Div('','spacer'));
+  this.append(this.property.createTree());
 }
 
 inherit(Property, Component, [
@@ -871,6 +896,15 @@ inherit(Property, Component, [
     if (attrs !== this.attrs) {
       this.key.removeClass(this.attrs);
       this.key.addClass(attrs);
+    }
+    if (this.prop) {
+      var prop = this.mirror.get(this._key);
+      if (prop.subject !== this.prop.subject) {
+        this.prop = prop;
+        this.replace(this.property, renderer.render(prop));
+      } else if (this.property.tree) {
+        this.property.tree.refresh();
+      }
     }
   }
 ]);
@@ -886,7 +920,10 @@ function Proto(mirror){
   this.key.addClass(this.attrs);
   this.append(this.key);
   this.property = mirror.getPrototype();
-  this.append(renderer.render(this.property));
+  var prop = renderer.render(this.property);
+  this.append(prop);
+  this.append(new Div('','spacer'));
+  this.append(prop.createTree());
 }
 
 inherit(Proto, Property, [
@@ -899,6 +936,33 @@ inherit(Proto, Property, [
     }
   }
 ]);
+
+function PreviewProperty(mirror, key){
+  Component.call(this, 'li');
+  this.mirror = mirror;
+  this.attrs = attributes[mirror.propAttributes(key)];
+  this.addClass('preview-property');
+  this.key = new Key(key);
+  this.key.addClass(this.attrs);
+  this.append(this.key);
+  this.prop = mirror.get(key);
+  this.property = previewRenderer.render(this.prop);
+  this.append(this.property);
+}
+
+inherit(PreviewProperty, Property);
+
+
+function Index(mirror, index){
+  Component.call(this, 'li');
+  this.mirror = mirror;
+  this.addClass('index');
+  this.prop = mirror.get(index);
+  this.property = previewRenderer.render(this.prop);
+  this.append(this.property);
+}
+
+inherit(Index, PreviewProperty);
 
 function Label(kind){
   Component.call(this, 'div');
@@ -918,66 +982,10 @@ inherit(Label, Component, [
 ]);
 
 
-function Leaf(mirror){
-  Component.call(this, 'div');
-  this.mirror = mirror;
-  this.addClass('leaf');
-  this.label = new Label(mirror.kind);
-  this.append(this.label);
-  this.refresh();
-}
-
-Leaf.create = function create(mirror){
-  return new Leaf(mirror);
-}
-
-inherit(Leaf, Component, [
-  function refresh(){
-    this.label.text(this.mirror.label());
-    return this;
-  }
-]);
-
-
-
-function StringLeaf(mirror){
-  Leaf.call(this, mirror);
-}
-
-StringLeaf.create = function create(mirror){
-  return new StringLeaf(mirror);
-}
-
-inherit(StringLeaf, Leaf, [
-  function refresh(){
-    this.label.text(utility.quotes(this.mirror.subject));
-    return this;
-  },
-]);
-
-
-function NumberLeaf(mirror){
-  Leaf.call(this, mirror);
-}
-
-NumberLeaf.create = function create(mirror){
-  return new NumberLeaf(mirror);
-}
-
-inherit(NumberLeaf, Leaf, [
-  function refresh(){
-    var label = this.mirror.label();
-    this.label.text(label === 'number' ? this.mirror.subject : label);
-    return this;
-  },
-]);
-
-
-
-
 function Tree(){
   Component.call(this, 'ul');
   this.addClass('tree');
+  this.hide();
   this.expanded = false;
 }
 
@@ -991,26 +999,35 @@ function replace(parent, element, replacement){
 inherit(Tree, Component, [
   function expand(){
     if (!this.expanded && this.emit('expand')) {
-      this.addClass('expanded');
       this.expanded = true;
+      this.show();
       return true;
     }
     return false;
   },
   function contract(){
     if (this.expanded && this.emit('contract')) {
-      this.removeClass('expanded');
       this.expanded = false;
+      this.hide();
       return true;
     }
     return false;
   },
   function toggle(){
     if (this.expanded) {
-      return this.contract();
+      this.contract();
+      return false;
     } else {
-      return this.expand();
+      this.expand();
+      return true;
     }
+  },
+  function refresh(){
+    var children = this.children || [];
+    for (var i=0; i < children.length; i++) {
+      children[i].refresh();
+    }
+    return this;
   },
   function forEach(callback, context){
     context = context || this;
@@ -1018,87 +1035,181 @@ inherit(Tree, Component, [
     for (var i=0; i < children.length; i++) {
       callback.call(context, children[i], i, this);
     }
+    return this;
   }
 ]);
+
+
+function creator(Ctor){
+  Ctor.create = function create(mirror){
+    var item = new Ctor(mirror);
+    item.refresh();
+    return item;
+  };
+}
+
+
+function Leaf(mirror){
+  Component.call(this, 'div');
+  this.mirror = mirror;
+  this.addClass('leaf');
+  this.label = new Label(mirror.kind);
+  this.append(this.label);
+}
+
+creator(Leaf);
+inherit(Leaf, Component, [
+  function refresh(){
+    this.label.text(this.mirror.label());
+    return this;
+  },
+  function createTree(){
+    return null;
+  }
+]);
+
+
+
+function StringLeaf(mirror){
+  Leaf.call(this, mirror);
+}
+
+creator(StringLeaf);
+inherit(StringLeaf, Leaf, [
+  function refresh(){
+    this.label.text(utility.quotes(this.mirror.subject));
+    return this;
+  },
+]);
+
+
+function NumberLeaf(mirror){
+  Leaf.call(this, mirror);
+}
+
+creator(NumberLeaf);
+inherit(NumberLeaf, Leaf, [
+  function refresh(){
+    var label = this.mirror.label();
+    this.label.text(label === 'number' ? this.mirror.subject : label);
+    return this;
+  },
+]);
+
 
 
 function Branch(mirror){
-  var self = this,
-      initialized;
   Component.call(this, 'div');
   this.mirror = mirror;
-  this.label = new Label(mirror.kind);
-  this.append(this.label);
   this.addClass('branch');
-  this.tree = new Tree;
-  this.append(this.tree);
-  this.tree.on('expand', function(e){
-    if (!initialized) {
-      initialized = true;
-      mirror.list(true).forEach(function(key){
-        this.append(new Property(mirror, key));
-      }, this);
-      this.append(new Proto(mirror));
-    } else {
-      this.forEach(function(item){
-        item.refresh();
-      }, this);
-    }
-  });
-  this.refresh();
-
-  this.label.on('click', function(e){
-    self.tree.toggle();
-  });
+  this.label = this.append(this.createLabel());
+  this.preview = this.append(this.createPreview());
 }
 
-Branch.create = function create(mirror){
-  return new Branch(mirror);
-}
-
+creator(Branch);
 inherit(Branch, Component, [
+  function createLabel(){
+    var label = new Label(this.mirror.kind);
+    label.on('click', function(e){
+      if (this.tree) {
+        this.tree.expanded ? this.contract() : this.expand();
+      }
+    }, this);
+    return label;
+  },
+  function createTree(){
+    return this.tree = new Tree;
+  },
+  function createPreview(){
+    return previewRenderer.render(this.mirror);
+  },
+  function expand(){
+    if (!this.tree.expanded) {
+      this.expanded();
+      this.addClass('expanded');
+      this.tree.expand();
+    }
+    return this;
+  },
+  function contract(){
+    if (this.tree.expanded) {
+      this.contracted();
+      this.removeClass('expanded');
+      this.tree.contract();
+    }
+    return this;
+  },
+  function expanded(){
+    this.preview.hide();
+    if (!this.tree.initialized) {
+      this.initTree();
+    } else {
+      this.tree.refresh();
+    }
+    return this;
+  },
+  function contracted(){
+    this.preview.show();
+    return this;
+  },
   function refresh(){
     this.label.text(this.mirror.label());
+    return this;
+  },
+  function initTree(){
+    if (!this.tree.initialized) {
+      this.tree.initialized = true;
+      this.keys = this.mirror.list(true);
+      this.keys.forEach(function(key){
+        this.tree.append(new Property(this.mirror, key));
+      }, this);
+      this.tree.append(new Proto(this.mirror));
+    }
+    return this;
   }
 ]);
+
 
 
 function FunctionBranch(mirror){
   Branch.call(this, mirror);
-
-  var name = this.mirror.getName(),
-      params = this.mirror.getParams();
-
-  if (params.rest) {
-    params.push('...'+params.pop());
-  }
-
-  this.label.append(new Span(name, 'FunctionName'));
-  var paramContainer = new Span('', 'Params');
-  for (var i=0; i < params.length; i++) {
-    paramContainer.append(new Span(params[i], 'Param'))
-  }
-  this.label.append(paramContainer);
 }
 
-FunctionBranch.create = function create(mirror){
-  return new FunctionBranch(mirror);
-}
-
+creator(FunctionBranch);
 inherit(FunctionBranch, Branch, [
-  function refresh(){ }
-]);
+  function createLabel(){
+    var label = Branch.prototype.createLabel.call(this),
+        name = this.mirror.getName(),
+        params = this.mirror.getParams();
 
+    if (params.rest) {
+      params.push('...'+params.pop());
+    }
+
+    label.append(new Span(name, 'FunctionName'));
+    var container = new Span('', 'Params');
+    for (var i=0; i < params.length; i++) {
+      container.append(new Span(params[i], 'Param'))
+    }
+    label.append(container);
+    return label;
+  },
+  function createPreview(){
+    return new Preview(this.mirror);
+  },
+  function refresh(){
+    this.preview && this.preview.refresh();
+    this.tree && this.tree.refresh();
+    return this;
+  }
+]);
 
 
 function ThrownBranch(mirror){
   Branch.call(this, mirror);
 }
 
-ThrownBranch.create = function create(mirror){
-  return new ThrownBranch(mirror);
-}
-
+creator(ThrownBranch);
 inherit(ThrownBranch, Branch, [
   function refresh(){
     this.label.innerHTML = '';
@@ -1106,9 +1217,10 @@ inherit(ThrownBranch, Branch, [
     this.label.append(new Div(this.mirror.getError(), 'Exception'));
     this.label.append(new Div(this.mirror.getValue('scope'), 'Scope'));
     this.label.append(new Div(this.mirror.getValue('code'), 'Code'));
+    this.preview && this.preview.refresh();
+    this.tree && this.tree.refresh();
   }
 ]);
-
 
 var renderer = new debug.Renderer({
   Unknown: Branch.create,
@@ -1136,6 +1248,86 @@ var renderer = new debug.Renderer({
   WeakMap: Branch.create
 });
 
+
+
+
+function Preview(mirror){
+  Component.call(this, 'ul');
+  this.mirror = mirror;
+  this.addClass('preview');
+  this.createPreview();
+}
+
+creator(Preview);
+inherit(Preview, Branch, [
+  function createPreview(){
+    this.mirror.list(false).forEach(function(key){
+      this.append(new PreviewProperty(this.mirror, key));
+    }, this);
+  },
+  function refresh(){
+    var children = this.children || [];
+    for (var i=0; i < children.length; i++) {
+      children[i].refresh();
+    }
+    return this;
+  },
+]);
+
+
+function IndexedPreview(mirror){
+  Preview.call(this, mirror);
+  this.addClass('ArrayPreview');
+}
+
+creator(IndexedPreview);
+inherit(IndexedPreview, Preview, [
+  function createPreview(){
+    var len = this.mirror.getValue('length');
+    for (var i=0; i < len; i++) {
+      this.append(new Index(this.mirror, i));
+    }
+  }
+]);
+
+
+function FunctionPreview(mirror){
+  Preview.call(this, mirror);
+  this.addClass('FunctionPreview');
+}
+
+creator(FunctionPreview);
+inherit(FunctionPreview, Preview, [
+  function createPreview(){
+    this.text(this.mirror.getName());
+  }
+]);
+
+var previewRenderer = new debug.Renderer({
+  Unknown: Preview.create,
+  BooleanValue: Leaf.create,
+  StringValue: StringLeaf.create,
+  NumberValue: NumberLeaf.create,
+  UndefinedValue: Leaf.create,
+  NullValue: Leaf.create,
+  Global: Preview.create,
+  Thrown: Preview.create,
+  Arguments: Preview.create,
+  Array: IndexedPreview.create,
+  Boolean: Preview.create,
+  Date: Preview.create,
+  Error: Preview.create,
+  Function: FunctionPreview.create,
+  JSON: Preview.create,
+  Map: Preview.create,
+  Math: Preview.create,
+  Object: Preview.create,
+  Number: Preview.create,
+  RegExp: Preview.create,
+  Set: Preview.create,
+  String: Preview.create,
+  WeakMap: Preview.create
+});
 
 
 
@@ -1370,14 +1562,32 @@ inherit(Editor, Component, [
   }
 ]);
 
+function Result(result){
+  Component.call(this, 'li');
+  this.result = this.append(result);
+  var tree = result.createTree();
+  if (tree) {
+    this.append(document.createElement('div'));
+    this.append(tree);
+  }
+}
+
+inherit(Result, Component, [
+  function refresh(){
+    this.result.refresh();
+  }
+]);
+
 //var input = new InputBox({ hint: 'Enter code to run...', autofocus: true }),
 var body = new Component(document.body),
     stdout = new Console,
-    root = _('div'),
+    inspector = new Tree,
     input = new Editor,
     instructions = new Instructions;
 
-root.className = 'root';
+inspector.removeClass('tree');
+inspector.addClass('inspector');
+inspector.show();
 
 var main = new Panel(null, {
   name: 'container',
@@ -1399,7 +1609,7 @@ var main = new Panel(null, {
     right: {
       label: 'Inspector',
       name: 'view',
-      content: root
+      content: inspector
     },
   },
   bottom: {
@@ -1411,14 +1621,10 @@ var main = new Panel(null, {
 
 
 function runInContext(code, realm){
-  if (root.firstChild) {
-    root.removeChild(root.firstChild);
-  }
   var result = renderer.render(realm.evaluate(code));
-  root.appendChild(result.element);
-  if (result.tree) {
-    result.tree.expand();
-  }
+  inspector.append(new Result(result));
+  inspector.element.scrollTop = inspector.element.scrollHeight;
+  inspector.refresh();
   return result;
 }
 
