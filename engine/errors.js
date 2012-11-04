0fbfe04d0b9e41eee796b1c6fdc20a7800befe68
Brandon Benvie <brandon@bbenvie.com>
2012-11-04 06:40:55 -0500

    general incompatible object message

--- engine/errors.js
+++ engine/errors.js
@@ -175,7 +175,7 @@ var errors = (function(errors, messages, exports){
     invalid_super_binding               : ["object has no super binding"],
     not_generic                         : ["$0", " is not generic and was called on an invalid target"],
     spread_non_object                   : ["Expecting an object as spread argument, but got ", "$0"],
-    incompatible_array_iterator         : ["$0", " called on incompatible object"]
+    called_on_incompatible_object       : ["$0", " called on incompatible object"]
   },
   ReferenceError: {
     unknown_label                  : ["Undefined label '", "$0", "'"],
